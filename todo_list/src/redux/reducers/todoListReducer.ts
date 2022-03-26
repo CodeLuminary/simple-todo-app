4e9c96c1a7dc685afd58153673b2ae6be1cc0da2
CodeLuminary <victorijoni@yahoo.com>
2022-03-26 10:11:09 +0100

    todoListReducer setup

--- todo_list/src/redux/reducers/todoListReducer.ts
+++ todo_list/src/redux/reducers/todoListReducer.ts
@@ -1,14 +1,23 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import type { RootState } from '../store';
+//import type { RootState } from '../store';
 
 // Define a type for the slice state
-interface CounterState {
-  value: number
-}
+
+interface list{
+  id: number,
+  task: string,
+  is_completed: boolean,
+  created_at: string,
+  completed_at: string
+};
+
+interface ToDoState {
+  value: list[];
+};
 
 // Define the initial state using that type
-const initialState: CounterState = {
-  value: 0
+const initialState: ToDoState = {
+  value: []
 }
 
 export const todoListSlice = createSlice({
@@ -16,22 +25,16 @@ export const todoListSlice = createSlice({
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    increment: state => {
-      state.value += 1
-    },
-    decrement: state => {
-      state.value -= 1
-    },
     // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
+    setTodoList: (state, action: PayloadAction<list[]>) => {
+      state.value = action.payload
     }
   }
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { setTodoList } = todoListSlice.actions
 
 // Other code such as selectors can use the imported `RootState` type
 //export const selectCount = (state: RootState) => state.counter.value
 
-export default counterSlice.reducer
+export default todoListSlice.reducer
