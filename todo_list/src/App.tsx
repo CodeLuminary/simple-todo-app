ac4b554fc99a0b84915489d0e6b78a50441624ed
CodeLuminary <victorijoni@yahoo.com>
2022-03-26 14:51:34 +0100

    Project UI setup

--- todo_list/src/App.tsx
+++ todo_list/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './css/App.css';
+import {useSelector, useDispatch} from "react-redux";
 
 function App() {
   return (
     <div className="App">
-      
+      <div></div>
+      <div>
+        <div>
+          <input className="input" type="text" />
+          <button className="btn">Add Task</button>
+          <div className="table">
+          </div>     
+        </div>
+      </div>
     </div>
   );
 }
