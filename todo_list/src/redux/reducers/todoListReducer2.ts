3c0b6b20d59be013a2e224bf1afd284ec8db8f68
CodeLuminary <victorijoni@yahoo.com>
2022-03-26 17:25:36 +0100

    Project done

--- todo_list/src/redux/reducers/todoListReducer2.ts
+++ todo_list/src/redux/reducers/todoListReducer2.ts
@@ -1,9 +1,15 @@
+/*
+    This is just a replica of todoListReducer but all actions apart for the
+    setTodoList action will be O(1) computation.
+*/
+
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 //import type { RootState } from '../store';
 
 // Define a type for the slice state
 
 let counter: number = 0;
+//let length: number = 0;
 
 interface list{
   id?: number,
@@ -35,7 +41,10 @@ export const todoListSlice = createSlice({
   reducers: {
     // Use the PayloadAction type to declare the contents of `action.payload`
     setTodoList: (state, action: PayloadAction<list[]>) => {
-      state.value = action.payload
+      state.value = action.payload;
+      for(let x in action.payload){
+          state.number_of_list +=1;
+      }
     },
     addToList: (state, action: PayloadAction<list>) =>{
         counter++;
@@ -48,10 +57,11 @@ export const todoListSlice = createSlice({
             created_at: (new Date()).toString()
           }
         }
-      
+        state.number_of_list +=1;
     },
     removeFromList: (state, action: PayloadAction<list>)=>{
       //state.value = state.value.filter( item => item.id !== action.payload.id)
+      state.number_of_list -=1;
     },
     setTaskAsCompleted: (state, action: PayloadAction<list>)=>{  
         let id = action.payload.id;
