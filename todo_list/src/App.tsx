29ff6e226d5846538cc86ba0b86e4935112c7d4e
CodeLuminary <victorijoni@yahoo.com>
2022-03-26 15:08:53 +0100

    UseDispatch & UseSelector setup for App.tsx

--- todo_list/src/App.tsx
+++ todo_list/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './css/App.css';
-import {useSelector, useDispatch} from "react-redux";
+import {TypedUseSelectorHook, useSelector, useDispatch} from "react-redux";
+import type { RootState, AppDispatch } from './redux/store';
 
 function App() {
+  const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+  const todoList = useAppSelector((state)=> state.list);
+  const useAppDispatch = () => useDispatch<AppDispatch>()
+  const dispatch = useAppDispatch();
+
   return (
     <div className="App">
       <div></div>
@@ -11,6 +17,7 @@ function App() {
           <input className="input" type="text" />
           <button className="btn">Add Task</button>
           <div className="table">
+              
           </div>     
         </div>
       </div>
